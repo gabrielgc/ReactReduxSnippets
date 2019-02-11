fb21f6844f3e81e1c08b11dd15e5c2d651a94d1c
gabriel cardoso <gabriel@blueforge.com.br>
2019-02-11 22:25:47 +0000

    Add project files.

---
+++ react-snippet/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace react_snippet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FolderTextbox = new System.Windows.Forms.TextBox();
+            this.ActionTypesTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateButton.Location = new System.Drawing.Point(12, 258);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(582, 42);
+            this.CreateButton.TabIndex = 0;
+            this.CreateButton.Text = "Create";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            this.CreateButton.Click += new System.EventHandler(this.CreateButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Folder name (CamelCase)";
+            // 
+            // FolderTextbox
+            // 
+            this.FolderTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FolderTextbox.Location = new System.Drawing.Point(36, 90);
+            this.FolderTextbox.Name = "FolderTextbox";
+            this.FolderTextbox.Size = new System.Drawing.Size(201, 30);
+            this.FolderTextbox.TabIndex = 2;
+            // 
+            // ActionTypesTextbox
+            // 
+            this.ActionTypesTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ActionTypesTextbox.Location = new System.Drawing.Point(36, 186);
+            this.ActionTypesTextbox.Name = "ActionTypesTextbox";
+            this.ActionTypesTextbox.Size = new System.Drawing.Size(201, 30);
+            this.ActionTypesTextbox.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 149);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Action Types (split using  ;  )";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(299, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Enough copying & pasting";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(243, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(135, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Eg: ForgotPassword";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(243, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(205, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Eg: INIT;BEGIN;SUCCESS;END";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(606, 338);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ActionTypesTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FolderTextbox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreateButton);
+            this.Name = "Form1";
+            this.Text = "React Redux Snippet - v 0.0.1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FolderTextbox;
+        private System.Windows.Forms.TextBox ActionTypesTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ react-snippet/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace react_snippet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> ActionTypesFormated { get; set; }
+
+        private void CreateButton_Click(object sender, EventArgs e)
+        {
+            var folder = FolderTextbox.Text;
+            var actionTypes = ActionTypesTextbox.Text.ToUpper().Split(';').ToList();
+            GenerateSnippets(folder, actionTypes);
+        }
+
+        private void GenerateSnippets(string folder, List<string> actionTypes)
+        {
+            var actionTypesFile = CreateActionTypes(folder, actionTypes);
+            // Create the file.
+            using (var fs = File.Create($"{folder}ActionTypes.js"))
+            {
+                var info = new UTF8Encoding(true).GetBytes(actionTypesFile);
+                fs.Write(info, 0, info.Length);
+            }
+
+            var reducer = CreateReducer(folder, actionTypes);
+            // Create the file.
+            using (var fs = File.Create($"{folder}Reducer.js"))
+            {
+                var info = new UTF8Encoding(true).GetBytes(reducer);
+                fs.Write(info, 0, info.Length);
+            }
+
+            var initialState = CreateInitialState();
+            using (var fs = File.Create($"{folder}InitialState.js"))
+            {
+                var info = new UTF8Encoding(true).GetBytes(initialState);
+                fs.Write(info, 0, info.Length);
+            }
+
+            var action = CreateActions(folder, actionTypes);
+            using (var fs = File.Create($"{folder}Action.js"))
+            {
+                var info = new UTF8Encoding(true).GetBytes(action);
+                fs.Write(info, 0, info.Length);
+            }
+
+            MessageBox.Show("Check local folder", "Done", MessageBoxButtons.OK);
+        }
+
+        private string CreateActions(string folderCamel, List<string> actionTypes)
+        {
+            var sb = new StringBuilder();
+            var folderLower = char.ToLower(folderCamel[0]) + folderCamel.Substring(1);
+            var import1 = $@"import * as Types from './{folderCamel}ActionTypes';";
+            sb.AppendLine(import1).AppendLine();
+            int i = 0;
+            foreach (var item in actionTypes)
+            {
+                var exportFunction = $@"export function {folderLower}{ToCamelCase(item)}() {{";
+                var returnType = $@"    return {{type: Types.{ActionTypesFormated[i]}}};";
+                sb.AppendLine(exportFunction);
+                sb.AppendLine(returnType).AppendLine("}").AppendLine();
+                i++;
+            }
+
+            return sb.ToString();
+        }
+
+        private string ToCamelCase(string text)
+        {
+            return char.ToUpper(text[0]) + text.Substring(1).ToLower();
+        }
+        private string CreateInitialState()
+        {
+            var exportDefault = @"export default {";
+            var isBusy = @"    isBusy: false";
+            var sb = new StringBuilder();
+            sb.AppendLine(exportDefault);
+            sb.AppendLine(isBusy).AppendLine("}");
+            return sb.ToString();
+        }
+
+        private string CreateReducer(string folderCamel, List<string> actionTypes)
+        {
+            var folderLower = char.ToLower(folderCamel[0]) + folderCamel.Substring(1);
+            var import1 = $@"import * as Types from './{folderCamel}ActionTypes';";
+            var import2 = $@"import initialState from './{folderCamel}InitialState';";
+
+            var exportDefault = $@"export default function {folderLower}Reducer(state = initialState, action) {{";
+            var initSwitch = $@"    switch (action.type) {{";
+            var returnItem = $@"            return {{ ...state, isBusy: }};";
+
+            var sb = new StringBuilder();
+            sb.AppendLine(import1);
+            sb.AppendLine(import2).AppendLine();
+            sb.AppendLine(exportDefault);
+            sb.AppendLine(initSwitch);
+            foreach (var item in ActionTypesFormated)
+            {
+                var caseItem = $@"      case Types.{item}:";
+                sb.AppendLine(caseItem);
+                sb.AppendLine(returnItem);
+            }
+
+            var defaultItem = $@"       default:";
+            var defaultReturn = $@"     return state;";
+            sb.AppendLine(defaultItem).AppendLine(defaultReturn).AppendLine(@"  }").AppendLine(@"}");
+            return sb.ToString();
+        }
+
+        private string CreateActionTypes(string folderCamel, List<string> actionTypes)
+        {
+            ActionTypesFormated = new List<string>();
+            var folder_snake = string.Concat(folderCamel.Select((x, i) => i > 0
+                                                                          && char.IsUpper(x)
+                ? "_" + x.ToString()
+                : x.ToString())).ToUpper();
+            var sb = new StringBuilder();
+            foreach (var item in actionTypes)
+            {
+                var placeHolder = $@"export const {folder_snake}_{item} = '{folderCamel}/{item}';";
+                ActionTypesFormated.Add($@"{folder_snake}_{item}");
+                sb.AppendLine(placeHolder);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
